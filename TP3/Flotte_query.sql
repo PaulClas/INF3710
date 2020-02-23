9a0ba202d0491c9d28316d9814b71fbeae42ba33
Sid Ali Guermache <sid-ali.guermache@polymtl.ca>
2020-02-23 16:43:09 -0500

    query a et b

--- TP3/Flotte_query.sql
+++ TP3/Flotte_query.sql
@@ -1,13 +1,14 @@
 ﻿--1) (1 point) Retourner le nombre d'appareils produits par pays.
-select idConstructeur, count(*) as nbappareils from Avion
-group by pays, Constructeur
-order by nbappareils desc, pays
+select Count(*) as nbApp, c.pays
+from avion a inner join constructeur c on a.idConstructeur = c.idConstructeur
+group by c.idConstructeur, c.pays
 --2) (1.5 point) Retourner le nombre d'appareils produits par pays dont le nombre de sièges est supérieur à 200.
-select idConstructeur, count(*) as nbappareils from Avion
+select Count(*) as nbApp, c.pays
+from avion a inner join constructeur c on a.idConstructeur = c.idConstructeur
 WHERE
 nbSieges > 200
-group by pays, Constructeur
-order by nbappareils desc, pays
+group by c.idConstructeur, c.pays
+
 --3)(1 point) Préparer une requête afin de réaliser une analyse de l’état de la flotte à partir des données suivantes : le modèle, le nombre de sièges, le nombre d’heures accumulées et la vitesse.
 SELECT idMoteur.modele AS MODELE from Moteur,
 idAvion.nbSieges AS NBSIEGES from Avion, 
--- TP3/Flotte_schema.sql
+++ TP3/Flotte_schema.sql
@@ -3,32 +3,29 @@ SET search_path = flotteDB;
 DROP SCHEMA IF EXISTS FLOTTEDB CASCADE;
 CREATE SCHEMA FLOTTEDB;
 
-CREATE DOMAIN FLOTTEDB.typeConstructeur AS CHAR
-	CHECK (VALUE IN ('Motoriste', 'avionneur', 'systemes'));
-
 CREATE TABLE FLOTTEDB.Constructeur (
     idConstructeur      SERIAL              NOT NULL,
-    nomCompagnie        VARCHAR(20)         NOT NULL,
-    type_constructeur   typeConstructeur    NOT NULL,
-    pays                VARCHAR(20)         NOT NULL,
+    nomCompagnie        VARCHAR(150)         NOT NULL,
+    type_constructeur   VARCHAR(20) CONSTRAINT typeConstructeur CHECK (type_constructeur IN ('Motoriste', 'Avionneur', 'Systemes')),
+    pays                VARCHAR(150)         NOT NULL,
     
     PRIMARY KEY (idConstructeur)
 );
 
-CREATE TABLE Avion (
+CREATE TABLE FLOTTEDB.Avion (
     idAvion         SERIAL     NOT NULL,
-    modele          VARCHAR(20)     NOT NULL,
+    modele          VARCHAR(50)     NOT NULL,
     idConstructeur  SERIAL          NOT NULL,
     nbMoteurs       INT             NOT NULL,
     nbSieges        INT             NOT NULL,
     PRIMARY KEY (idAvion),
     FOREIGN KEY (idConstructeur) REFERENCES Constructeur(idConstructeur));
 
-CREATE TABLE Moteur (
+CREATE TABLE FLOTTEDB.Moteur (
     idMoteur        SERIAL        NOT NULL,
-    modele          VARCHAR(20)     NOT NULL,
+    modele          VARCHAR(50)     NOT NULL,
     idConstructeur  SERIAL          NOT NULL,
-    diametre        VARCHAR(20)     NOT NULL,
+    diametre        VARCHAR(50)     NOT NULL,
     longueur        INT             NOT NULL,
     poids           INT             NOT NULL,
     poussee         INT             NOT NULL,
@@ -36,14 +33,13 @@ CREATE TABLE Moteur (
     FOREIGN KEY (idConstructeur) REFERENCES Constructeur(idConstructeur)
 );
 
-CREATE TABLE Flotte (
-    noSerieAvion    SERIAL        NOT NULL,
-    noSerieMoteur   SERIAL        NOT NULL,
+CREATE TABLE FLOTTEDB.Flotte (
+    noSerieAvion    VARCHAR(50)        NOT NULL,
+    noSerieMoteur   VARCHAR(50)        NOT NULL,
     idMoteur        SERIAL        NOT NULL,
     idAvion         SERIAL        NOT NULL,
     vitesse         INT           NOT NULL,
     autonomie       NUMERIC(3, 1) NOT NULL,
-    poussee         INT           NOT NULL,
     dateService     DATE          NOT NULL,
     heuresVol       INT,
     dateRetrait     DATE,
